2cf944fc8a4d1a753eaeb71f085509138e57dba2
jackiewen24 <vanquangqui241092@gmail.com>
2021-01-27 23:02:52 +0700

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs = require('hbs');
 const getWeather = require('./../utils/getWeather');
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 // Define paths
 const publicDirPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
@@ -61,6 +61,6 @@ app.get('*', (req, res) => {
     res.send('404 Page');
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
